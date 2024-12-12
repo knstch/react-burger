738d3ee24c29ca9ac24f5207695e94b2daef1a42
k.cherepanov <k.cherepanov@sberauto.com>
2024-12-12 21:34:38 +0700

    inited project

---
+++ src/components/Header/ConstructorBtn/constructorBtn.tsx
@@ -0,0 +1,12 @@
+import {BurgerIcon} from '@ya.praktikum/react-developer-burger-ui-components'
+
+const ConstructorHeaderBtn = () => {
+    return (
+        <button>
+            <BurgerIcon type="primary" />
+            <span>Конструктор</span>
+        </button>
+    )
+}
+
+export default ConstructorHeaderBtn
---
+++ src/components/Header/header.tsx
@@ -0,0 +1,13 @@
+import ConstructorHeaderBtn from './ConstructorBtn/constructorBtn'
+
+const Header = () => {
+    return (
+        <div>
+            <div>
+                <ConstructorHeaderBtn/>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
