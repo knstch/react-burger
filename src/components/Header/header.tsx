dcf59544f2b49da5c1b16f589bd224c8a74d9c8e
k.cherepanov <k.cherepanov@sberauto.com>
2024-12-13 12:56:33 +0700

    сделал хедер

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,13 +1,49 @@
-import ConstructorHeaderBtn from './ConstructorBtn/constructorBtn'
+import {BurgerIcon, ListIcon, Logo, ProfileIcon} from "@ya.praktikum/react-developer-burger-ui-components";
+import styles from './header.module.css';
+import React, {FC} from "react";
+import {TIconProps} from "@ya.praktikum/react-developer-burger-ui-components/dist/ui/icons/utils";
 
 const Header = () => {
+    const orderBtns: MenuItemProps[] = [
+        {
+            icon: BurgerIcon,
+            text: "Конструктор",
+            link: ""
+        },
+        {
+            icon: ListIcon,
+            text: "Лента заказов",
+            link: ""
+        },
+    ]
+
     return (
-        <div>
-            <div>
-                <ConstructorHeaderBtn/>
+        <div className={styles.header}>
+            <div className={styles.btnGroup}>
+                {orderBtns.map((item, i) => (
+                    <MenuItem key={i} icon={item.icon} link={item.link} text={item.text}/>
+                ))}
             </div>
+            <Logo className={styles.logo} />
+            <MenuItem icon={ProfileIcon} text={"Личный кабинет"} link={""}/>
         </div>
     )
 }
+
+interface MenuItemProps {
+    icon:  FC<TIconProps>;
+    text: string;
+    link: string;
+}
+
+const MenuItem: React.FC<MenuItemProps> = (props) => {
+    return (
+        <button className={`${styles.headerBtn} text text_type_main-default`}>
+            <props.icon type={"primary"}></props.icon>
+            <span>{props.text}</span>
+        </button>
+    )
+}
+
 
 export default Header;
